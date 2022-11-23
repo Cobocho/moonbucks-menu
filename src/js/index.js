f335864081a392ab6941d93adc68de5a9e05ff5f
Cobocho <rlaalsrb1111@naver.com>
2022-11-24 01:25:42 +0900

    feat: 메뉴 삭제 confirm 추가

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ const $menuForm = document.getElementById("espresso-menu-form");
 const $menuInput = document.getElementById("espresso-menu-name");
 const $menuAddButton = document.getElementById("espresso-menu-submit-button");
 const $menuList = document.getElementById("espresso-menu-list");
-const $menuCount = document.querySelector(".menu-count");
 
 $menuForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -22,8 +21,18 @@ function addMenu() {
     document.querySelectorAll(".menu-remove-button").length - 1
   ];
   removeBtn.addEventListener("click", (event) => {
-    event.target.parentNode.remove();
-    countReset();
+    if (confirm("메뉴를 삭제하시겠습니까?")) {
+      event.target.parentNode.remove();
+      countReset();
+    }
+  });
+  const editBtn =
+    document.querySelectorAll(".menu-edit-button")[
+      document.querySelectorAll(".menu-edit-button").length - 1
+    ];
+  editBtn.addEventListener("click", (event) => {
+    event.target.parentNode.querySelector(".menu-name").textContent =
+      prompt("바꿀 메뉴명을 입력해주세요!");
   });
   countReset();
   $menuInput.value = "";
